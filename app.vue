86aa67ceb36b4505ddedd9382783759246b4f820
robertbudijr <valenta_abram_29rpl@student.smktelkom-mlg.sch.id>
2023-07-02 09:10:32 +0700

    feat: left sidebar complete

--- app.vue
+++ app.vue
@@ -1,5 +1,22 @@
+<script setup>
+const darkMode = ref(false);
+</script>
+
 <template>
-  <div>
-    <h1 class="text-blue-700">Twitterx</h1>
+  <div :class="{ dark: darkMode }">
+    <div class="bg-white dark:bg-dim-900">
+      <div class="min-h-full">
+        <div class="grid grid-cols-12 mx-auto sm:px-6 lg:max-w-7xl lg:px-8 lg:gap-5">
+          <!-- Left Sidebar -->
+          <div class="hidden md:block xs:col-span-1 xl:col-span-2">
+            <div class="sticky top-0">
+              <SidebarLeft />
+            </div>
+          </div>
+          <!-- Main Content -->
+          <!-- Right Sidebar -->
+        </div>
+      </div>
+    </div>
   </div>
 </template>
